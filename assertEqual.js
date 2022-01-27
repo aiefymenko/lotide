f7ff2622b54c3c163e0f2b409163fa81de44d29b
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-01-27 17:48:47 -0500

    Another attempt

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = (actual, expected) => {
-  return (actual !== expected) ? console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`) : console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  return (actual === expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
